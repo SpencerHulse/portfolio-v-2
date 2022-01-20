8c8037b1635ab512cca063ce303d8c0643124701
Spencer Hulse <hulse.spencer@gmail.com>
2022-01-20 01:42:32 -0500

    enhanced project links using css and jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav-link");
+const portfolioContainer = $(".portfolio-container");
+const portfolioItem = $(".portfolio-item");
+const portfolioInfoToggle = $(".portfolio-info");
 
 let toggleNav = () => {
   document.body.classList.toggle("nav-open");
@@ -9,8 +12,23 @@ let closeNav = () => {
   document.body.classList.remove("nav-open");
 };
 
+let toggleInfoOn = (event) => {
+  let link = $(event.target.closest(".portfolio-item"));
+  let paragraph = $(link.children()[1]).addClass("portfolio-info-toggle");
+};
+
+let toggleInfoOff = (event) => {
+  let link = $(event.target.closest(".portfolio-item"));
+  let paragraph = $(link.children()[1]).removeClass("portfolio-info-toggle");
+};
+
 navToggle.addEventListener("click", toggleNav);
 //the event listener is attached to each element with the nav-link class
 navLinks.forEach((link) => {
   link.addEventListener("click", closeNav);
 });
+
+portfolioContainer.on("mouseover", toggleInfoOn);
+portfolioContainer.on("mouseout", toggleInfoOff);
+portfolioItem.focusin(toggleInfoOn);
+portfolioItem.focusout(toggleInfoOff);
